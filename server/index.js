fdc410cd56879b30436c4db65970ef97acdab559
Clement <kwasiclement764@gmail.com>
2025-06-06 22:10:11 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:["http://localhost:5173", "https://clempixels-learning-platform.onrender.com"],
+    origin:"https://clempixels-learning-platform.onrender.com",
     credentials:true
 }));
  
