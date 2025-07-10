298a416b144a4ec9d4bfc2a7778f12acdedcb351
Clement <kwasiclement764@gmail.com>
2025-07-10 01:46:20 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"https://clempixels-learning-platform.onrender.com",
+    origin:["https://clempixels-learning-platform.onrender.com", "https://mern-lms-hazel.vercel.app"],
     credentials:true
 }));
  
