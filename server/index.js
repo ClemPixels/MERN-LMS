5e27cd131b170a4ab8103d74b49b0ec2a95d0bff
Clement <kwasiclement764@gmail.com>
2025-09-21 19:27:52 +0000

    cors:3

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin:["https://clempixels-learning-platform.onrender.com", "http://localhost:5173"],
+    origin:["https://clempixels-learning-platform.onrender.com", "https://442learn.vercel.app", "http://localhost:5173"],
     credentials:true
 }));
 
